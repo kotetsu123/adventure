d6bcab852a7f92e10594ddd0b8eea05b0cae8764
kotetsu123 <116703571+kotetsu123@users.noreply.github.com>
2023-08-28 18:40:09 +0800

    23/08/28

--- Assets/Resources/Scripts/MainCharacter_Movement.cs
+++ Assets/Resources/Scripts/MainCharacter_Movement.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class MainCharacter_Movement : MonoBehaviour
 {
+    private Rigidbody2D rigidbody2D;
+
     public Sprite[] CharacterSprite;
     public int MoveSpeed = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Movement();
-        
+
+    }
+    private void FixedUpdate()
+    {
+       
     }
     private void Movement()
     {
@@ -44,10 +54,17 @@ public class MainCharacter_Movement : MonoBehaviour
             position.y = position.y - 0.1f;
             transform.position = position;
         }*/
-        float v = Input.GetAxisRaw("Vertical");//垂直移动
+        /*float v = Input.GetAxisRaw("Vertical");//垂直移动
         transform.Translate(Vector3.up * MoveSpeed *v * Time.fixedDeltaTime, Space.World);
         float h = Input.GetAxisRaw("Horizontal");//水平移动
         transform.Translate(Vector3.right* MoveSpeed * h * Time.fixedDeltaTime, Space.World);
-        
+        Vector3 position= transform.position;
+        rigidbody2D.position = position;*/
+        Vector2 position=transform.position;
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        position.x = position.x+3*h * Time.fixedDeltaTime;
+        position.y=position.y+3*v* Time.fixedDeltaTime;
+        rigidbody2D.position = position;
     }
 }

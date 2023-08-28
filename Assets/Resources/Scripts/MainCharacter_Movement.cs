5f24b8f979ab61a39f683cf5ef6d472a9790c897
kotetsu123 <116703571+kotetsu123@users.noreply.github.com>
2023-08-28 21:17:30 +0800

    23/08/28

--- Assets/Resources/Scripts/MainCharacter_Movement.cs
+++ Assets/Resources/Scripts/MainCharacter_Movement.cs
@@ -6,16 +6,20 @@ using UnityEngine;
 public class MainCharacter_Movement : MonoBehaviour
 {
     private Rigidbody2D rigidbody2D;
+    private int currentHp;//角色当前生命值
+    
 
     public Sprite[] CharacterSprite;
     public int MoveSpeed = 0;
+    public int MaxHp=5;//角色最大生命值上限
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-        
+        currentHp = MaxHp;
     }
 
     // Update is called once per frame
@@ -23,6 +27,7 @@ public class MainCharacter_Movement : MonoBehaviour
     {
 
         Movement();
+        HP_Control(0);
 
     }
     private void FixedUpdate()
@@ -65,6 +70,11 @@ public class MainCharacter_Movement : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         position.x = position.x+3*h * Time.fixedDeltaTime;
         position.y=position.y+3*v* Time.fixedDeltaTime;
-        rigidbody2D.position = position;
+        rigidbody2D.MovePosition(position);
+    }
+    private void HP_Control(int amount)
+    {
+        currentHp = Mathf.Clamp(currentHp + amount, 0, MaxHp);      
+        Debug.Log(currentHp+"/"+MaxHp);
     }
 }
